7a3e4223f7302556ac365860823e3881d3462263
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-01-18 01:42:34 +0200

    Style book_list bottom app bar partially

--- lib/presentation/screens/book_list.dart
+++ lib/presentation/screens/book_list.dart
@@ -5,6 +5,93 @@ class BookList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book list'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 8.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: const <Widget>[
+                      Text('Book 1'),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text('Author 1'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 8.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: const <Widget>[
+                      Text('Book 2'),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text('Author 2'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: const FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                IconButton(
+                  padding: EdgeInsets.all(0),
+                  onPressed: null,
+                  icon: Icon(Icons.home),
+                ),
+                Text('Home'),
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                IconButton(
+                  padding: EdgeInsets.all(0),
+                  onPressed: null,
+                  icon: Icon(Icons.show_chart_rounded),
+                ),
+                Text('Stats'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
