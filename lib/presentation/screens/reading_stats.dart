136ad7a4df75ac4120dd958e7c50418408856796
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-01-20 16:55:09 +0200

    Update cards from stats screen

--- lib/presentation/screens/reading_stats.dart
+++ lib/presentation/screens/reading_stats.dart
@@ -10,20 +10,46 @@ class ReadingStats extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Card(
-            child: Container(
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                image: const DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/creative_color_book.png'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        '102',
+                        style: TextStyle(fontSize: 48.0),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          //color: Colors.white,
+                          image: DecorationImage(
+                            fit: BoxFit.fitWidth,
+                            image: AssetImage(
+                                'assets/images/creative_color_book.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('You have 52 books you want to read!'),
-              ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'books you want to read!',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+              ],
             ),
             margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
           ),
@@ -31,25 +57,40 @@ class ReadingStats extends StatelessWidget {
             height: 10.0,
           ),
           Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
             child: Column(
               children: <Widget>[
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    image: const DecorationImage(
-                      fit: BoxFit.contain,
-                      image: AssetImage('assets/images/stack_of_books.png'),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        '52',
+                        style: TextStyle(fontSize: 48.0),
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          //color: Colors.white,
+                          image: DecorationImage(
+                            fit: BoxFit.fitWidth,
+                            image:
+                                AssetImage('assets/images/stack_of_books.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 const Padding(
                   padding: EdgeInsets.all(10.0),
                   child: Text(
-                    'You read 102 books!',
+                    'books read!',
                     style: TextStyle(
                       fontSize: 24.0,
                     ),
