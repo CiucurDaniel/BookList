892f9db02b0a781f57637e541a51d8834c9e45f0
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-01-31 21:17:57 +0200

    Remove print call and unused import

--- lib/cubits/book_cubit.dart
+++ lib/cubits/book_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:book_list/data/models/book.dart';
 import 'package:book_list/data/repositories/book_repository_interface.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'book_state.dart';
 
@@ -15,12 +14,6 @@ class BooksCubit extends Cubit<BooksState> {
     try {
       emit(const BooksLoading());
       final books = await bookRepository.getAllBooks();
-      print('I GOT THE BOOKS');
-      print('I GOT THE BOOKS');
-      print('I GOT THE BOOKS');
-      print('I GOT THE BOOKS');
-      print('The length is ${books.length}');
-      print(books);
       emit(BooksLoaded(books));
     } catch (error) {
       emit(const BooksLoadedWithError());
