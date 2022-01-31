529cf4abcd61338111e47cfc075e4b026814efef
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-01-31 22:25:59 +0200

    Implement stats Cubit

--- lib/cubits/stats_cubit.dart
+++ lib/cubits/stats_cubit.dart
@@ -1,8 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:book_list/data/repositories/book_repository_interface.dart';
 import 'package:equatable/equatable.dart';
+import 'package:book_list/data/models/book.dart';
 
 part 'stats_state.dart';
 
 class StatsCubit extends Cubit<StatsState> {
-  StatsCubit() : super(StatsInitial());
+  StatsCubit({required this.bookRepository}) : super(StatsInitial());
+
+  final IBookRepository bookRepository;
+
+  void getUserStats() async {
+    emit(StatsLoading());
+
+    try {
+      final booksRead = await bookRepository.getAllBooks().then((value) =>
+          value.where((element) => element.status == Status.read).length);
+
+      final booksWantingToRead = await bookRepository.getAllBooks().then(
+          (value) => value
+              .where((element) => element.status == Status.wantToRead)
+              .length);
+
+      emit(StatsLoaded(booksRead, booksWantingToRead));
+    } catch (error) {
+      emit(StatsLoadedWithError());
+    }
+  }
 }
--- lib/cubits/stats_state.dart
+++ lib/cubits/stats_state.dart
@@ -8,3 +8,23 @@ class StatsInitial extends StatsState {
   @override
   List<Object> get props => [];
 }
+
+class StatsLoading extends StatsState {
+  @override
+  List<Object?> get props => [];
+}
+
+class StatsLoaded extends StatsState {
+  final int bookRead;
+  final int bookWantingToRead;
+
+  const StatsLoaded(this.bookRead, this.bookWantingToRead);
+
+  @override
+  List<Object?> get props => [bookRead, bookWantingToRead];
+}
+
+class StatsLoadedWithError extends StatsState {
+  @override
+  List<Object?> get props => [];
+}
