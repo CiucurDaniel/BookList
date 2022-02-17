9c20a9c480a7f7e79acd8714b121bf85fe88c906
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-02-18 00:32:55 +0200

    Implement details screen

--- lib/presentation/screens/book_details.dart
+++ lib/presentation/screens/book_details.dart
@@ -14,14 +14,66 @@ class BookDetails extends StatelessWidget {
         title: const Text('Book details'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Text(book.title),
-          Text(book.author),
-          Text(
-            book.pages.toString(),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/creative_color_book.png'),
+            Row(
+              children: <Widget>[
+                const Expanded(
+                  child: Text('Book title: '),
+                ),
+                Text(book.title),
+              ],
+            ),
+            const SizedBox(height: 16.0,),
+            Row(
+              children: <Widget>[
+                const Expanded(
+                  child: Text('Author: '),
+                ),
+                Text(book.author),
+              ],
+            ),
+            const SizedBox(height: 16.0,),
+            Row(
+              children: <Widget>[
+                const Expanded(
+                  child: Text('Pages: '),
+                ),
+                Text(
+                  book.pages.toString(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0,),
+            Row(
+              children: <Widget>[
+                const Expanded(
+                  child: Text('Status: '),
+                ),
+                Text(
+                  book.status.toString(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0,),
+            Row(
+              children: const <Widget>[
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('Edit'),
+                ),
+                FlatButton(
+                  onPressed: null,
+                  child: Text('Delete book'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
